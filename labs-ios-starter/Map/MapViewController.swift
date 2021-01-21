e906a228dc1a3b5420f4e014cad8b8f0104a8a7f
Claudia Maciel <clc_80@yahoo.com>
2021-01-20 18:54:14 -0800

    Added MapView Controller

---
+++ labs-ios-starter/Map/MapViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MapViewController.swift
+//  labs-ios-starter
+//
+//  Created by Claudia Maciel on 1/20/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class MapViewController: UIViewController {
+
+    @IBOutlet var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

6a3a3c1e4f5f826c64c755704bd2ca83de640758
XNUConner <72773002+XNUConner@users.noreply.github.com>
2021-02-08 19:14:17 -0600

    Completed cities vc on storyboard, created CitiesVC file

---
+++ labs-ios-starter/Cities/CitiesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CitiesViewController.swift
+//  labs-ios-starter
+//
+//  Created by Conner on 2/8/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+
+import UIKit
+
+class CitiesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

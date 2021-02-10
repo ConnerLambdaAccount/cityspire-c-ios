09fced5c15211e8b64da66309b5d260b99a2dfdd
XNUConner <72773002+XNUConner@users.noreply.github.com>
2021-02-09 18:32:57 -0600

    Search functionality added to cities tab, user does not have to search directly on the map. Each result is displayed in a cell.
    Storyboard: Fixed constraint conflict

--- labs-ios-starter/Cities/CitiesViewController.swift
+++ labs-ios-starter/Cities/CitiesViewController.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class CitiesViewController: UIViewController {
     @IBOutlet var citiesCollectionView: UICollectionView!
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var cities: [String] = []
+    var citySearchResults: [String] = []
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
         citiesCollectionView.dataSource = self
         citiesCollectionView.delegate = self
+        cities = parseCities()
+        citySearchResults = cities
+        searchBar.delegate = self
     }
 }
 
 extension CitiesViewController: UICollectionViewDelegate, UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cityCell", for: indexPath) as! CityCollectionViewCell
-        cell.cityStateLabel.text = "Chicago, IL"
+        
+        let city: String = citySearchResults[indexPath.row]
+        let cityName: String = String(city.dropLast(3))
+        let state: String = String(city.dropFirst(city.count-2))
+
+        cell.cityStateLabel.text = "\(cityName), \(state)"
         cell.populationLabel.text = "Population: 2.7M"
         cell.cityImageView.image = UIImage(named: "chicago")
         cell.cityImageView.contentMode = .scaleToFill
@@ -28,6 +42,16 @@ extension CitiesViewController: UICollectionViewDelegate, UICollectionViewDataSo
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 6
+        return citySearchResults.count
+    }
+}
+
+extension CitiesViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        let filteredCities = cities.filter { (city) -> Bool in
+            return city.lowercased().starts(with: searchText.lowercased())
+        }
+        self.citySearchResults = filteredCities
+        self.citiesCollectionView.reloadData()
     }
 }

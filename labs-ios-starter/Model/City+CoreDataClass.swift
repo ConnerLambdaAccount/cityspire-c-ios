cdae05578d5514a1805ad11c061b2bbfe188957a
XNUConner <72773002+XNUConner@users.noreply.github.com>
2021-02-11 21:47:16 -0600

    Added City placeholder model .xcdatamodeld file and generated NSManagedObject subclass

---
+++ labs-ios-starter/Model/City+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  City+CoreDataClass.swift
+//  labs-ios-starter
+//
+//  Created by Conner on 2/11/21.
+//  Copyright © 2021 Spencer Curtis. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(City)
+public class City: NSManagedObject {
+
+}
